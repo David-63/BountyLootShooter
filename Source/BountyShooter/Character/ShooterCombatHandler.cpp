e6f6dedbefc65e2373a24703b2fdd84e02827454
David-63 <jiseop.63@gmail.com>
2024-12-12 16:56:51 +0900

    안쓰는 에셋 정리

--- Source/BountyShooter/Character/ShooterCombatHandler.cpp
+++ Source/BountyShooter/Character/ShooterCombatHandler.cpp
@@ -2,6 +2,10 @@
 
 
 #include "ShooterCombatHandler.h"
+#include "ShooterCharacter.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "InputActionValue.h"
 
 // Sets default values for this component's properties
 UShooterCombatHandler::UShooterCombatHandler()
@@ -30,5 +34,53 @@ void UShooterCombatHandler::TickComponent(float DeltaTime, ELevelTick TickType, 
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
 	// ...
+}
+
+void UShooterCombatHandler::MappingCombatContext(AShooterCharacter* TargetCharacter)
+{
+	ShooterCharacter = TargetCharacter;
+
+	// Check that the character is valid, and has no weapon component yet
+	if (ShooterCharacter == nullptr || ShooterCharacter->GetInstanceComponents().FindItemByClass<UShooterMovementHandler>()) return;
+
+	// Set up action bindings
+	if (APlayerController* PlayerController = Cast<APlayerController>(ShooterCharacter->GetController()))
+	{
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+		{
+			// Set the priority of the mapping to 1, so that it overrides the Jump action with the Fire action when using touch input
+			Subsystem->AddMappingContext(IMC_MovementHandler, 1);
+		}
+
+		if (UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent>(PlayerController->InputComponent))
+		{
+			EnhancedInputComponent->BindAction(FireAction, ETriggerEvent::Triggered, this, &UShooterCombatHandler::Fire);
+			EnhancedInputComponent->BindAction(MeleeAction, ETriggerEvent::Triggered, this, &UShooterCombatHandler::Melee);
+			EnhancedInputComponent->BindAction(ThrowAction, ETriggerEvent::Triggered, this, &UShooterCombatHandler::Throw);
+			EnhancedInputComponent->BindAction(ReloadAction, ETriggerEvent::Triggered, this, &UShooterCombatHandler::ChamberingRound);
+			EnhancedInputComponent->BindAction(ReloadAction, ETriggerEvent::Triggered, this, &UShooterCombatHandler::AmmoInsertion);
+			
+		}
+	}
+}
+
+void UShooterCombatHandler::ChamberingRound()
+{
+}
+
+void UShooterCombatHandler::AmmoInsertion()
+{
+}
+
+void UShooterCombatHandler::Fire()
+{
+}
+
+void UShooterCombatHandler::Melee()
+{
+}
+
+void UShooterCombatHandler::Throw()
+{
 }
 
--- Source/BountyShooter/Character/ShooterCombatHandler.h
+++ Source/BountyShooter/Character/ShooterCombatHandler.h
@@ -6,6 +6,9 @@
 #include "Components/ActorComponent.h"
 #include "ShooterCombatHandler.generated.h"
 
+class AShooterCharacter;
+class UInputAction;
+struct FInputActionValue;
 
 UCLASS( ClassGroup=(ShooterHandler), meta=(BlueprintSpawnableComponent) )
 class BOUNTYSHOOTER_API UShooterCombatHandler : public UActorComponent
@@ -24,5 +27,33 @@ public:
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
-		
+	UFUNCTION(BlueprintCallable)
+	void MappingCombatContext(AShooterCharacter* TargetCharacter);
+
+private:
+	UPROPERTY()
+	TObjectPtr<AShooterCharacter> ShooterCharacter;
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Move Input")
+	class UInputMappingContext* IMC_MovementHandler;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Move Input")
+	TObjectPtr<UInputAction> FireAction;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Move Input")
+	TObjectPtr<UInputAction> MeleeAction;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Move Input")
+	TObjectPtr<UInputAction> ThrowAction;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Move Input")
+	TObjectPtr<UInputAction> ReloadAction;
+
+public:
+	void ChamberingRound();
+	void AmmoInsertion();
+	void Fire();
+	void Melee();
+	void Throw();
 };
